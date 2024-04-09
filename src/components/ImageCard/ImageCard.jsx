21ebccf1009cb93bd97e46d2c38dcb4a0294404f
ChernyshovVadym <vad14@i.ua>
2024-04-09 17:35:51 +0300

    add

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,9 +1,12 @@
+import css from "./ImageCard.module.css";
+
 const ImageCard = ({ image, openModal }) => {
   return (
-    <div>
+    <div className={css.big}>
       <img
         src={image.urls.small}
         alt={image.alt_description}
+        className={css.img}
         onClick={() => openModal(image.urls.regular, image.alt_description)}
       />
     </div>
